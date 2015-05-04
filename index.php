6e48c01c7a5ee7a9925a1c0e28f6e51fd2021dd1
John Breslin <johnbreslin@Johns-Mac-Pro.local>
2015-05-03 20:27:23 -0400

    First commit, adding index

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+
+	<!-- John Ink © 2015 www.johnink.com www.barelyanimated.com-->
+	<head>
+		<title>OhMusing! Ideas on how to find ideas</title>
+		<meta charset="UTF-8" />
+		<meta name="description" content="A website dedicated to helping you find ideas. Providing tools and games to help you explore your mind and see what you find."/>
+		<meta name="keywords" content="ideas, self improvement, play, creative" />
+		<meta name="author" content="John Ink" />
+	</head>
+
+
+
+	<body>
+		<header>OhMusing!</header>
+
+
+
+
+	</body>
+
+
+
+
+
+
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
