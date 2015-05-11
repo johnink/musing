43f5db7797d0f6d161549de348c2980f76a2ed75
John Breslin <johneatspie@gmail.com>
2015-05-11 10:30:44 -0400

    Separated repeated header into head file, added css files

--- index.php
+++ index.php
@@ -4,14 +4,10 @@
 	<!-- John Ink © 2015 www.johnink.com www.barelyanimated.com-->
 	<head>
 		<title>OhMusing! Ideas on how to find ideas</title>
-		<meta charset="UTF-8" />
 		<meta name="description" content="A website dedicated to helping you find ideas. Providing tools and games to help you explore your mind and see what you find."/>
 		<meta name="keywords" content="ideas, self improvement, play, creative" />
-		<meta name="author" content="John Ink" />
+		<?php include("php/head.php") ?>
 
-		<!--temp nofollow -->
-		<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
-		<!--temp nofollow -->
 	</head>
 
 
---
+++ php/head.php
@@ -0,0 +1,18 @@
+<? 
+
+//base head content on every page
+
+echo '
+	<meta name="author" content="John Ink" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="UTF-8" />
+	<link rel="stylesheet" type="style/css" href="style/normalize.css" />
+	<link rel="stylesheet" type="style/css" href="style/base.css" />
+
+
+		<!--temp nofollow -->
+		<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
+		<!--temp nofollow -->
+
+'
+?>
