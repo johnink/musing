a43a26cebb376227eaf42049a06d3a7c192f2c02
JohnInk <johneatspie@gmail.com>
2015-05-28 03:12:28 -0700

    started the get started page

---
+++ getstarted.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+
+	<!-- John Ink © 2015 www.johnink.com www.barelyanimated.com-->
+	<head>
+		<title>OhMusing! Getting Started</title>
+		<meta name="description" content="A website dedicated to helping you find ideas. Providing tools and games to help you explore your mind and see what you find."/>
+		<meta name="keywords" content="ideas, self improvement, play, creative" />
+		<?php include("php/head.php") ?>
+
+	</head>
+
+	<body>
+		<header>OhMusing!</header>
+		<div id="jayz">
+			<div id="get_started">
+
+				<!--An explaination of what this site is and what its for-->
+				<h1>What's the problem?</h1>
+				<p>
+					Ideas can be hard to find. Especially, today on this content driven web, because search engines tend to give priority to sites that update regularly. There exist many people are able to generate new content every single day. How can a person stay relevent?
+				</p>
+				<h1>Who we are</h1>
+				<p>
+					It's something I've struggled with, and I'm sure plenty of others have, too. It's what this site is dedicated to helping with. This site cataloges little improv games you can play in your free time to stimulate your imagination and help find ideas.
+
+					Sound like something you could use? Ready to get started? Let's go.
+				</p>
+
+				<h1>What you'll need...</h1>
+				<h2>A notebook</h2>
+				<p>
+					You never know when inspiration will strike. So, it's important to be ready with paper. I prefer <a href="http://www.moleskine.com/us/">Moleskine notebooks</a> for durability.
+				</p>
+
+				<!--Start an account-->
+				<!--Popular games-->
+
+			</div>
+
+		</div>
+
+
+
+	</body>
+
+	<footer>
+		<?php include("php/foot.php") ?>
+
+	</footer>
+
+
+
+
+
+
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- index.php
+++ index.php
@@ -1,16 +1,14 @@
 <!DOCTYPE html>
 <html>
 
-	<!-- John Ink © 2015 www.johnink.com www.barel
-
-	</head>yanimated.com-->
+	<!-- John Ink © 2015 www.johnink.com www.barelyanimated.com-->
 	<head>
 		<title>OhMusing! Ideas on how to find ideas</title>
 		<meta name="description" content="A website dedicated to helping you find ideas. Providing tools and games to help you explore your mind and see what you find."/>
 		<meta name="keywords" content="ideas, self improvement, play, creative" />
 		<?php include("php/head.php") ?>
 
-
+	</head>
 
 	<body>
 		<header>OhMusing!</header>
