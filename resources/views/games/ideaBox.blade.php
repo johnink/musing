ca042e5678e5950b8e1784cbc88c349501cabe8e
John Breslin <johneatspie@gmail.com>
2016-03-07 08:01:57 -0500

    Added Idea Box

---
+++ resources/views/games/ideaBox.blade.php
@@ -0,0 +1,25 @@
+
+<div id="long_desc">
+	<h2>Basic Idea</h2>
+	<p>Brainstorming can be a little bit easier if you start with a box. Draw a rectangle on a piece of paper. Then write a topic of your choosing in the middle.</p>
+	<img src="/games/ideaBox/oneboxempty.png" alt="Funny looking fat animals" /><br /><br />
+	<p>Now, fill all the remaining white space with ideas.</p>
+	<img src="/games/ideaBox/oneboxfull.png" alt="Funny looking fat animals" /><br /><br />
+	<p>Feeling confident? Make a bigger box. Or try to fill a whole page. Here's some more examples.</p>
+	<a href="/games/ideaBox/allboxes.png"><img src="/games/ideaBox/allboxes.png" alt="More examples" style="width:90%;max-width:828px"/><p>(click to enlarge)</p></a>
+
+</div>
+
+<div id="good_prompts">
+	<ul>
+		<lh>More Prompts to get you started</lh>
+		<li>Bad Pet Names</li>
+		<li>Money Saving Tips</li>
+		<li>Completing a ghost's unfinished business</li>
+	</ul>
+	<br /><br />
+	<ul>
+		<lh>Good Sources of Prompts</lh>
+		<li><a href="http://amzn.to/1QBtNdA">'Game of Things' cards</a></li>
+	</ul>
+</div>
