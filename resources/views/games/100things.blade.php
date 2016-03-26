03a7dea57402a4320a24b313b24d997ec5537124
System Administrator <johneatspie@gmail.com>
2016-03-26 08:18:44 -0400

    New game: 100 things

---
+++ resources/views/games/100things.blade.php
@@ -0,0 +1,57 @@
+<style>
+ #variations img{
+ 	width:100%;
+ 	max-width:530px;
+ 	margin:1em 0;
+ }
+</style>
+
+<div id="long_desc">
+	<h2>Basic Idea</h2>
+	<p>When doing exercises, a hundred is a nice round number to play with. Take a <a href="/game/prompter">prompt</a> and doodle one hundred things. Keep a tally at the top of your page.</p>
+
+</div>
+
+<div id="variations">
+	<h2>Variations</h2>
+	<p>You can take an unstructured approach.</p>
+
+	<a href="/games/100things/pagebottle.png"><img src="/games/100things/pagebottle.png" alt="Example of less structured approach" /></a>
+
+	<p>
+
+	<p>Alternatively, the book <a href="http://amzn.to/1VNTwlj" target="_blank">Creative Workshop</a> features a variation where you draw on a 10x10 grid.</p>
+
+	<a href="http://amzn.to/1VNTwlj" target="_blank"><img src="/games/100things/creativeworkshop.png" alt="Example from Creative Workshop" /></a>
+
+	<p><blockquote style="font-size:1em;color:grey"><a href="http://amzn.to/1VNTwlj" target="_blank">Creative Workshop</a> is a book of project-oriented design exercises. If your aim is to work as a professional designer, this is a book worth looking into. Buying from the link below helps support Omusing.com.<br /><br/>
+
+	<iframe style="width:120px;height:240px;margin-left:3em" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=omusing-20&marketplace=amazon&region=US&placement=1600617972&asins=1600617972&linkId=e8cfbb74181a64e9a1bdf3c7ce7052cb&show_border=true&link_opens_in_new_window=true"></iframe>
+
+	</blockquote></p>
+
+	<p>You can also try to add a hundred details to a scene, like your own <i>Where's Waldo</i>:</p>
+
+	<a href="/games/100things/pagemall.png"><img src="/games/100things/pagemall.png" alt="Example of a scene." /></a>
+
+	<p>Or, just make a complete mess. Who are you trying to impress?</p>
+
+	<a href="/games/100things/pagemonster.png"><img src="/games/100things/pagemonster.png" alt="Another example of a scene." /></a>
+
+	<p>Good Luck, and keep exploring.</p>
+
+</div>
+
+<div id="good_prompts">
+	<ul>
+		<lh>Good Sources of Prompts</lh>
+		<li>Things you need practice drawing</li>
+		<li><a href="/game/prompter">Prompter</a></li>
+	</ul>
+	<ul>
+		<lh>More Prompts to get you started</lh>
+		<li>A great battle between Dentists and Vampires</li>
+		<li>A mad scientist's kitchen</li>
+		<li>A logo for yourself</li>
+	</ul>
+</div>
