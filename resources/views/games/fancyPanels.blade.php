7acf6001f0b943a1c3fb38af3c31bab756d401eb
System Administrator <johneatspie@gmail.com>
2016-06-10 22:06:22 -0400

    New Game : Fancy Panels (update)

--- resources/views/games/fancyPanels.blade.php
+++ resources/views/games/fancyPanels.blade.php
@@ -2,13 +2,13 @@
 	<h2>Basic Idea</h2>
 		<p>When you're stuck, give your mind a place to start. Draw some fancy panels.</p>
 
-		<a href="/games/fancyPanels/fancypanelsempty.png" target="_blank" ><img src="/games/fancyPanels/fancypanelsempty.png" alt="Empty Panels" /></a>
+		<a href="/games/fancyPanels/fancypanelsempty.png" target="_blank" ><img src="/games/fancyPanels/fancypanelsempty.png" alt="Empty Panels" class="game_img" /></a>
 
 		<p>Then try to make a comic that fits.</p>
 
-		<a href="/games/fancyPanels/fancypanelsfilled.png" target="_blank" ><img src="/games/fancyPanels/fancypanelsfilled.png" alt="Filled Panels" /></a>
+		<a href="/games/fancyPanels/fancypanelsfilled.png" target="_blank" ><img src="/games/fancyPanels/fancypanelsfilled.png" alt="Filled Panels" class="game_img" /></a>
 
-		<a href="/games/fancyPanels/fancypanelsexample.png" target="_blank" ><img src="/games/fancyPanels/fancypanelsexamples.png" alt="Empty Panels" /></a>
+		<a href="/games/fancyPanels/fancypanelsexample.png" target="_blank" ><img src="/games/fancyPanels/fancypanelsexamples.png" alt="Example Panels" class="game_img" /></a>
 
 
 </div>
