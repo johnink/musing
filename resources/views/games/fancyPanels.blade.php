57339428f6f34d9fe0cd31e5f6f8ec7f02eb7bb8
System Administrator <johneatspie@gmail.com>
2016-06-10 21:55:01 -0400

    New Game : Fancy Panels

---
+++ resources/views/games/fancyPanels.blade.php
@@ -0,0 +1,19 @@
+<div id="long_desc">
+	<h2>Basic Idea</h2>
+		<p>When you're stuck, give your mind a place to start. Draw some fancy panels.</p>
+
+		<a href="/games/fancyPanels/fancypanelsempty.png" target="_blank" ><img src="/games/fancyPanels/fancypanelsempty.png" alt="Empty Panels" /></a>
+
+		<p>Then try to make a comic that fits.</p>
+
+		<a href="/games/fancyPanels/fancypanelsfilled.png" target="_blank" ><img src="/games/fancyPanels/fancypanelsfilled.png" alt="Filled Panels" /></a>
+
+		<a href="/games/fancyPanels/fancypanelsexample.png" target="_blank" ><img src="/games/fancyPanels/fancypanelsexamples.png" alt="Empty Panels" /></a>
+
+
+</div>
+
+<div id="variations">
+	<h2>Variations</h2>
+	<p>Draw a lot of panels, or make it a single planel. Try to add panels until you reach a joke.</p>
+</div>
