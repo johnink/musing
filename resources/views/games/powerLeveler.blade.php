702992b2c0771ba12e498ccc87c7ec4e2ca51389
System Administrator <johneatspie@gmail.com>
2016-06-19 22:19:52 -0400

    New Game Power Leveler

--- resources/views/games/powerLeveler.blade.php
+++ resources/views/games/powerLeveler.blade.php
@@ -3,17 +3,17 @@
 <div id="long_desc">
 	<h2>Basic Idea</h2>
 	<p>In this game, to take something to it's logical extremes, as if it were a character in a roleplaying game. Find a prompt. For this example, we'll use "clock." Start from the most basic version you can think of for your subject, and make it level one.</p>
-	<a href="/games/powerLeveler/levelmaster_01.png" target="_blank" ><img src="/games/powerLeveler/levelmaster_01.png" target="_blank" alt="level 1 clock" style="width:70%;max-width:300px" /></a>
+	<a href="/games/powerLeveler/levelmaster_01.png" target="_blank" ><img src="/games/powerLeveler/levelmaster_01.png" target="_blank" alt="level 1 clock" style="width:90%;max-width:300px" /></a>
 	<p>From there, keep leveling your character. See how far you can take it.</p>
-	<a href="/games/powerLeveler/levelmaster_02.png" target="_blank" ><img src="/games/powerLeveler/levelmaster_02.png" target="_blank" alt="level 2 and 3 clock" style="width:70%;max-width:300px" /></a>
+	<a href="/games/powerLeveler/levelmaster_02.png" target="_blank" ><img src="/games/powerLeveler/levelmaster_02.png" target="_blank" alt="level 2 and 3 clock" style="width:90%;max-width:300px" /></a>
 
-	<a href="/games/powerLeveler/levelmaster_03.png" target="_blank" ><img src="/games/powerLeveler/levelmaster_03.png" target="_blank" alt="level 4 and 5 clock" style="width:70%;max-width:300px" /></a>
+	<a href="/games/powerLeveler/levelmaster_03.png" target="_blank" ><img src="/games/powerLeveler/levelmaster_03.png" target="_blank" alt="level 4 and 5 clock" style="width:90%;max-width:300px" /></a>
 
-	<a href="/games/powerLeveler/levelmaster_04.png" target="_blank" ><img src="/games/powerLeveler/levelmaster_04.png" target="_blank" alt="level 13 and 17 clock" style="width:70%;max-width:300px" /></a>
+	<a href="/games/powerLeveler/levelmaster_04.png" target="_blank" ><img src="/games/powerLeveler/levelmaster_04.png" target="_blank" alt="level 13 and 17 clock" style="width:90%;max-width:300px" /></a>
 
-	<a href="/games/powerLeveler/levelmaster_05.png" target="_blank" ><img src="/games/powerLeveler/levelmaster_05.png" target="_blank" alt="level 20 and 35 clock" style="width:70%;max-width:300px" /></a>
+	<a href="/games/powerLeveler/levelmaster_05.png" target="_blank" ><img src="/games/powerLeveler/levelmaster_05.png" target="_blank" alt="level 20 and 35 clock" style="width:90%;max-width:300px" /></a>
 
-	<a href="/games/powerLeveler/levelmaster_06.png" target="_blank" ><img src="/games/powerLeveler/levelmaster_06.png" target="_blank" alt="level 50 clock" style="width:70%;max-width:300px" /></a>
+	<a href="/games/powerLeveler/levelmaster_06.png" target="_blank" ><img src="/games/powerLeveler/levelmaster_06.png" target="_blank" alt="level 50 clock" style="width:90%;max-width:300px" /></a>
 
 
 </div>
