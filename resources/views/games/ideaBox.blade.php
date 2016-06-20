ac7fa491e9d6fe382e7706270173dedd4230fe14
System Administrator <johneatspie@gmail.com>
2016-06-19 22:03:42 -0400

    New Game Power Leveler

--- resources/views/games/ideaBox.blade.php
+++ resources/views/games/ideaBox.blade.php
@@ -2,9 +2,9 @@
 <div id="long_desc">
 	<h2>Basic Idea</h2>
 	<p>Brainstorming can be a little bit easier if you start with a box. Draw a rectangle on a piece of paper. Then write a topic of your choosing in the middle.</p>
-	<img src="/games/ideaBox/oneboxempty.png" alt="Funny looking fat animals" /><br /><br />
+	<img src="/games/ideaBox/oneboxempty.png" alt="Funny looking fat animals" class="postit" /><br /><br />
 	<p>Now, fill all the remaining white space with ideas.</p>
-	<img src="/games/ideaBox/oneboxfull.png" alt="Funny looking fat animals" /><br /><br />
+	<img src="/games/ideaBox/oneboxfull.png" alt="Funny looking fat animals" class="postit" /><br /><br />
 	<p>Feeling confident? Make a bigger box. Or try to fill a whole page. Here's some more examples.</p>
 	<a href="/games/ideaBox/allboxes.png"><img src="/games/ideaBox/allboxes.png" alt="More examples" style="width:90%;max-width:828px"/><p>(click to enlarge)</p></a>
 
